a722a159fe7b82ef4524766adb792f369966d940
Fais Nasrullah <fais@automatalabs.co>
2022-04-16 16:52:17 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:find_house_app/pages/splash_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: SplashPage(),
+    );
+  }
+}
